f492969b427f1d701b55b118ce0815911be96ba1
Dominik Ocsofszki <dominik.ocsofszki@smail.inf.h-brs.de>
2023-10-25 13:09:04 +0200

    adding vim.o options

---
+++ lua/config/vim_o.lua
@@ -0,0 +1,23 @@
+
+vim.o.termguicolors = true
+vim.o.number = true
+vim.o.hlsearch = true
+vim.o.mouse = "a"
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.cursorline = true
+vim.o.cursorlineopt = "number"
+vim.o.clipboard = "unnamedplus"
+vim.o.signcolumn ="yes"
+vim.o.hlsearch = false
+vim.o.breakindent = true
+
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = 'menuone,preview'
+-- vim.o.completeopt = 'menuone,noselect'
+vim.o.so = 15
+vim.o.tabstop = 4
